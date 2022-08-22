ac7c7eb28a54ec49558c26a1e52a3a484d2ac975
RealLinen <94056347+RealLinen@users.noreply.github.com>
2022-08-22 15:08:15 -0400

    Finihed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const EncodeNumber = require("./Encoder.js")
 const fs = require("fs")
 
-fs.writeFileSync("output.txt", EncodeNumber(400, { MaxChars: 1784, hide: true, lua_equation: true }))
+//fs.writeFileSync("output.txt", EncodeNumber(100, { MaxChars: 2500, hide: true, lua_equation: true }))
